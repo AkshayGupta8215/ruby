714420d270c0a4512d5447582c7c4e28b8709aa8
Akshay Gupta <akshaygupta8215@gmail.com>
2021-07-25 13:24:18 +0530

    Update SimpCalc.rb

--- SimpCalc.rb
+++ SimpCalc.rb
@@ -1,8 +1,8 @@
 print "Enter a simple equation\nOperations allowed are '+', '-', '*', '/' and '%'\nExample : \"1+3\"\nInput is : "
 a=Array.new
 a=gets
-b=a[0].to_i
-c=a[2].to_i
+b=a[0].to_f
+c=a[2].to_f
 print "Output is : "
 if a[1]=="+"
   puts b+c
